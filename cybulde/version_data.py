464690d93690360684a7e90cef96f72c380180d0
Tala Al Halabi <halabitala5@gmail.com>
2026-04-04 14:16:53 +0300

    run make version-data to initialize dvc

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,13 +1,15 @@
-from pathlib import Path "Path" is not accessed
+from pathlib import Path
 
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
 from cybulde.utils.data_utils import initialize_dvc
-from cybulde.utils.utils import get_logger "get_logger" is not accessed
+from cybulde.utils.utils import get_logger
+
 
 @get_config(config_path="../configs", config_name="config")
-def version_data(config: Config) -> None: "config" is not accessed
+def version_data(config: Config) -> None:
     initialize_dvc()
+
 
 if __name__ == "__main__":
     version_data()
